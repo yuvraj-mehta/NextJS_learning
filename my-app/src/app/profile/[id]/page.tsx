295518c9cc845ce5c8ddb1739926408b03148100
Yuvraj Mehta <yuvraj.mehta532@gmail.com>
2025-06-23 04:56:03 +0530

    Updates UserProfile to handle asynchronous params resolution
    Refactors UserProfile component to await the resolution of asynchronous params. Enhances flexibility by supporting promise-based input for user ID, ensuring compatibility with dynamic data fetching scenarios.

--- my-app/src/app/profile/[id]/page.tsx
+++ my-app/src/app/profile/[id]/page.tsx
@@ -1,11 +1,17 @@
-export default function UserProfile({ params }: { params: { id: string } }) {
+export default async function UserProfile({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const resolvedParams = await params;
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p className="text-4xl font-bold">
         This is the profile page for user ID:{" "}
-        <span className="text-blue-500">{params.id}</span>
+        <span className="text-blue-500">{resolvedParams.id}</span>
       </p>
     </div>
   );
